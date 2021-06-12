3f8ab4256cc3e329d30b01e012c97999e092f823
bananodroid <41457266+bananodroid@users.noreply.github.com>
2021-06-12 19:37:54 +1000

    22.1

--- enn/setup.sh
+++ enn/setup.sh
@@ -16,7 +16,7 @@ if lsof -Pi :80 -sTCP:LISTEN -t >/dev/null ; then
 fi
 
 echo "== Updating Docker images"
-sudo docker pull nanocurrency/nano:V22.0
+sudo docker pull nanocurrency/nano:V22.1
 sudo docker pull nanotools/nanonodemonitor
 
 echo "== Starting Docker containers"
